f86ecf0a70edc92c83b6fa2a333043b3cf2b0f15
rycont <reactdev@kakao.com>
2020-08-08 08:40:12 +0900

    feat : add NanumSqareRound webFont

--- src/components/GlobalStyle.ts
+++ src/components/GlobalStyle.ts
@@ -3,7 +3,7 @@ import styledReset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${styledReset}
-
+  @font-face { font-family: 'NanumSquareRound'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff'); font-weight: normal; font-style: normal; }
   * {
     -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
     font-family: 'NanumSquareRound', -apple-system, BlinkMacSystemFont,
