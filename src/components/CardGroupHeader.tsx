ebb83d78e2e1900d08391969674340dd74a288c3
rycont <reactdev@kakao.com>
2020-08-09 00:04:19 +0900

    feat : conditional subButton

--- src/components/CardGroupHeader.tsx
+++ src/components/CardGroupHeader.tsx
@@ -1,72 +1,72 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import css from '@emotion/css';
+import React from "react";
+import styled from "@emotion/styled";
+import css from "@emotion/css";
+import { Link } from "react-router-dom";
 
 interface IProps {
-  title: string;
-  withBubble: boolean;
-  subButton: {
+  withBubble?: boolean;
+  subButton?: {
     text: string;
     route?: string;
     action?: () => any;
-  }
+  };
 }
 
 const CardGroupHeader: React.FC<IProps> = ({
-  title,
   subButton,
   withBubble,
+  children,
 }) => {
   return (
     <Wrapper>
       <Header>
-        <Title withBubble={withBubble}>{title}</Title>
-        {subButton ?
-          subButton.route ?
-            <SubButton>
-              더보기
-            </SubButton>
-            :
-            <SubButton>
-              더보기
-            </SubButton>
-          : null}
+        <Title withBubble={withBubble}>{children}</Title>
+        {subButton ? (
+          subButton.route ? (
+            <Link to={subButton.route}>
+              <SubButton>{subButton.text}</SubButton>
+            </Link>
+          ) : (
+            <SubButton>{subButton.text}</SubButton>
+          )
+        ) : null}
       </Header>
     </Wrapper>
-  )
-}
+  );
+};
 
-const Title = styled.h1<{withBubble: boolean}>`
+const Title = styled.h1<{ withBubble?: boolean }>`
   font-size: 27px;
   font-weight: 900;
-  font-family: 'NanumSquare', sans-serif;
-  ${({ withBubble }) => withBubble && css`
-    &::after {
-      display: inline-block;
-      position: absolute;
-      width: 8px;
-      height: 8px;
-      border-radius: 4px;
-      background-color: #ff6359;
-      content: ' ';
-      margin-left: 4px;
-      margin-top: 3px;
-    }
-  `}
-`
+  font-family: "NanumSquare", sans-serif;
+  ${({ withBubble }) =>
+    withBubble &&
+    css`
+      &::after {
+        display: inline-block;
+        position: absolute;
+        width: 8px;
+        height: 8px;
+        border-radius: 4px;
+        background-color: #ff6359;
+        content: " ";
+        margin-left: 4px;
+        margin-top: 3px;
+      }
+    `}
+`;
 
 const Header = styled.div`
   display: flex;
-
-`
+`;
 const SubButton = styled.p`
   align-self: flex-end;
   margin-left: 15px;
   font-weight: 800;
   color: #8a8a8a;
-`
+`;
 const Wrapper = styled.section`
   margin-bottom: 14px;
-`
+`;
 
-export default CardGroupHeader
+export default CardGroupHeader;
