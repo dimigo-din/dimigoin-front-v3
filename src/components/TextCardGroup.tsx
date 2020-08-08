9981277b439317fa9a3193569658e2b2bfa8e781
rycont <reactdev@kakao.com>
2020-08-08 10:12:13 +0900

    feat : add spaceBetweenCards property to TextCardGroup 카드 사이에 여백을 주는 spaceBetweenCards 속성을 추가했습니다

--- src/components/TextCardGroup.tsx
+++ src/components/TextCardGroup.tsx
@@ -6,15 +6,39 @@ import css from '@emotion/css';
 interface IProps {
   content: (IDimiCard & {
     text: string
-  })[]
+  })[],
+  spaceBetweenCards?: boolean
 }
 
 const TextCardGroup: React.FC<IProps> = ({
-  content
+  content,
+  spaceBetweenCards
 }) => {
-  return <>
-    {content.map(e => ({...e, children: e.text})).map(Card)}
-  </>
+  return <Wrapper spaceBetweenCards={spaceBetweenCards}>
+    {content.map(e => ({
+      ...e,
+      children: e.text, //text로 받아온 카드의 값을 children으로 덮어씌웁니다
+
+      ...(!spaceBetweenCards && {
+        /* 카드 사이에 공간이 없게 한다면 호버효과를 강제로 끕니다
+          이 부분은 차후에 더 나은 코드로 수정해주세요.. */
+        hover: false
+      })
+    })).map(Card)}
+  </Wrapper>
 }
+
+const Wrapper = styled.section<{spaceBetweenCards?: boolean}>`
+  ${({ spaceBetweenCards }) => spaceBetweenCards ? css`
+      & div+div {
+        margin-top: 10px;
+      }
+  ` : css`
+      box-shadow: 0 0 20px 0 rgba(146, 146, 146, 0.09);
+      &>div {
+        box-shadow: none;
+      }
+    `}
+`
 
 export default TextCardGroup
