1390a6f0ab3506e95c537e77e7e7ecb92923d5be
rycont <reactdev@kakao.com>
2020-08-09 11:22:35 +0900

    feat : enhance layout experience

--- src/components/grids/Cols.tsx
+++ src/components/grids/Cols.tsx
@@ -8,12 +8,36 @@ export const Col = styled.div<{ width?: number }>`
       flex: ${width / 10};
     `}
 `;
-
-export const Divider = styled.div<{ visible?: boolean }>`
+export const Divider = styled.div<{ visible?: boolean; small?: boolean }>`
   margin: 0px 30px;
+  ${({ small }) =>
+    small &&
+    css`
+      margin: 0px 7.5px;
+    `}
   ${({ visible }) =>
     visible &&
     css`
       border: 1px solid #eeeeee;
     `}
+`;
+
+export const ResponsiveScreenWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 760px) {
+    flex-direction: column;
+    & > [class*="Divider"] {
+      margin: 30px 0px;
+    }
+  }
+`;
+
+export const ResponsiveWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 760px) {
+    flex-direction: column;
+    & > [class*="Divider"] {
+      margin: 30px 0px;
+    }
+  }
 `;
