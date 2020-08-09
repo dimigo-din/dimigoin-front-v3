bf1a5749078380c4251f66cdf66772baae2232ae
rycont <reactdev@kakao.com>
2020-08-09 11:20:37 +0900

    refactor : enhance readability => TextCardGroup

--- src/components/TextCardGroup.tsx
+++ src/components/TextCardGroup.tsx
@@ -16,37 +16,21 @@ const TextCardGroup: React.FC<IProps> = ({
   ...props
 }) => {
   return (
-    <Wrapper spaceBetweenCards={spaceBetweenCards} {...props}>
-      {content
-        .map((e) => ({
-          ...e,
-          children: e.text, //text로 받아온 카드의 값을 children으로 덮어씌웁니다
-
-          ...(!spaceBetweenCards && {
-            /* 카드 사이에 공간이 없게 한다면 호버효과를 강제로 끕니다
-          이 부분은 차후에 더 나은 코드로 수정해주세요.. */
-            hover: false,
-          }),
-        }))
-        .map(Card)}
-    </Wrapper>
+    <div css={spaceBetweenCards || shadow} {...props}>
+      {content.map((i) => (
+        <Card children={i.text} css={spaceBetweenCards || cancelHover} {...i} />
+      ))}
+    </div>
   );
 };
 
-const Wrapper = styled.section<{ spaceBetweenCards?: boolean }>`
-  ${({ spaceBetweenCards }) =>
-    spaceBetweenCards
-      ? css`
-          & div + div {
-            margin-top: 10px;
-          }
-        `
-      : css`
-          box-shadow: 0 0 20px 0 rgba(146, 146, 146, 0.09);
-          & > div {
-            box-shadow: none;
-          }
-        `}
+const shadow = css`
+  box-shadow: 0 0 20px 0 rgba(146, 146, 146, 0.09);
+`;
+
+const cancelHover = css`
+  box-shadow: none;
+  margin-top: 0px !important;
 `;
 
 export default TextCardGroup;
