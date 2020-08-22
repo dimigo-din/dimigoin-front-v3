a660b252d7edab0d7bbbb13d190dbe7fc2a624be
rycont <reactdev@kakao.com>
2020-08-22 15:13:54 +0900

    feat : extract TodayMeal Component

---
+++ src/components/TodayMeal.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "@emotion/styled";
+import DimiCard from "./dimiru/DimiCard";
+import css from "@emotion/css";
+
+const MealItem: React.FC<IMealItem> = ({
+  selected = false,
+  name = "",
+  menu = "",
+}) => (
+  <MealItemContainer selected={selected}>
+    <MealNameText selected={selected}>{name}</MealNameText>
+    <MealMenuText selected={selected}>{menu}</MealMenuText>
+  </MealItemContainer>
+);
+
+interface IProps {
+  meals: {
+    name: string;
+    menu: string;
+    selected?: boolean;
+  }[];
+}
+
+const TodayMeal: React.FC<IProps> = ({ meals }) => (
+  <MealCard>{meals.map(MealItem)}</MealCard>
+);
+
+interface IMealItemSelected {
+  selected?: boolean;
+}
+
+interface IMealItem extends IMealItemSelected {
+  name?: string;
+  menu?: string;
+}
+
+const MealCard = styled(DimiCard)`
+  height: 100%;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+`;
+const MealItemContainer = styled.div<IMealItemSelected>`
+  width: calc(100% - 78px);
+  flex-grow: 1;
+  border-left: 5px solid transparent;
+  padding-left: 34px;
+  padding-right: 39px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  ${({ selected = false }) =>
+    selected &&
+    css`
+      background-color: rgba(60, 112, 232, 0.04);
+      border-left-color: #3c70e8;
+    `}
+`;
+
+const MealNameText = styled.span<IMealItemSelected>`
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 1.17;
+  color: #d1d1d1;
+
+  ${({ selected = false }) =>
+    selected &&
+    css`
+      color: #3c70e8;
+    `}
+`;
+
+const MealMenuText = styled.p<IMealItemSelected>`
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 1.38;
+  word-break: keep-all;
+  margin-top: 12px;
+  color: #d1d1d1;
+
+  ${({ selected = false }) =>
+    selected &&
+    css`
+      color: #111111;
+    `}
+`;
+
+export default TodayMeal;
