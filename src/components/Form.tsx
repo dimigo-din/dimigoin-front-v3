1dc371db221997facd1aa9e9090a2a0f68449a24
rycont <reactdev@kakao.com>
2020-08-23 21:45:56 +0900

    feat : add FormHeader

---
+++ src/components/Form.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export const FormHeader = styled.h2`
+  font-size: 20px;
+  font-weight: 900;
+  margin-bottom: 18px;
+  &:not(:first-of-type) {
+    margin-top: 36px;
+  }
+`;
