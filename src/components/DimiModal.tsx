12ad0a7208c47adb38c1bb25d1f3df067c4316fa
rycont <reactdev@kakao.com>
2020-08-23 14:33:40 +0900

    feat : DimiModal

--- src/components/DimiModal.tsx
+++ src/components/DimiModal.tsx
@@ -1,50 +1,89 @@
 import React, { useState, useEffect, ReactNode } from "react";
 import styled from "@emotion/styled";
-import DimiCard from "./dimiru/DimiCard";
+import DimiCard, { IDimiCard } from "./dimiru/DimiCard";
+import css, { SerializedStyles } from "@emotion/css";
 
-// export let show: () => void;
-export let show: (el: (close: () => void) => ReactNode) => void;
+interface IModalProps {
+  cardProps?: IDimiCard & {
+    css?: SerializedStyles;
+  };
+  wrapperProps?: React.HTMLAttributes<HTMLDivElement>;
+  backdropProps?: React.HTMLAttributes<HTMLDivElement>;
+}
+
+export let show: (
+  el: (close: () => void) => ReactNode,
+  props?: IModalProps
+) => void;
 export const ModalContainer = () => {
   const [ModalElement, setModalElement] = useState<ReactNode>();
+  const [props, setProps] = useState<IModalProps>();
   const [visible, setVisivility] = useState(false);
+  const [disappearingAnimation, setDisappearingAnimation] = useState(false);
+  const disappear = () => {
+    setDisappearingAnimation(true);
+    setTimeout(() => setVisivility(false), 300);
+  };
   useEffect(() => {
-    show = (el: (close: () => void) => ReactNode) => {
-      setModalElement(el(() => setVisivility(false)));
+    show = (el: (close: () => void) => ReactNode, props?: IModalProps) => {
+      setModalElement(el(disappear));
       setVisivility(true);
+      setDisappearingAnimation(false);
+      if (props) setProps(props);
+      console.log(props);
     };
   }, []);
   return visible && ModalElement ? (
-    <FullBack onClick={() => setVisivility(false)}>
-      <ModalWrapper onClick={(e) => e.stopPropagation()}>
-        <Card>{ModalElement}</Card>
-      </ModalWrapper>
-    </FullBack>
+    <Backdrop
+      onClick={disappear}
+      visible={disappearingAnimation}
+      {...props?.backdropProps}
+    >
+      <Wrapper onClick={(e) => e.stopPropagation()} {...props?.wrapperProps}>
+        <Card {...props?.cardProps}>{ModalElement}</Card>
+      </Wrapper>
+    </Backdrop>
   ) : null;
 };
 
-const FullBack = styled.div`
+const Backdrop = styled.div<{ visible: boolean }>`
   position: absolute;
   width: 100vw;
   height: 100vh;
   z-index: 10;
-  /* let */
   display: flex;
   justify-content: space-between;
   backdrop-filter: blur(2px);
-  animation: appear 100ms forwards;
+  animation: appear 300ms forwards;
 
+  ${({ visible }) =>
+    visible &&
+    css`
+      animation: disappear 300ms forwards;
+    `}
   @keyframes appear {
     from {
       background-color: rgba(0, 0, 0, 0);
       visibility: hidden;
+      opacity: 0;
     }
     to {
       background-color: rgba(0, 0, 0, 0.1);
       visibility: visible;
     }
   }
+
+  @keyframes disappear {
+    from {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+    to {
+      background-color: rgba(0, 0, 0, 0);
+      opacity: 0;
+    }
+  }
 `;
-const ModalWrapper = styled.div`
+const Wrapper = styled.div`
   width: 720px;
   margin: auto;
 `;
