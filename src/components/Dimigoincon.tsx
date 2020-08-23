b86616ec6e3b280e9cc28f40d549a0478afb147d
rycont <reactdev@kakao.com>
2020-08-23 20:04:51 +0900

    feat : install dimigoincon

---
+++ src/components/Dimigoincon.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+import "dimigoincon";
+
+interface IDimigoincon {
+  className?: string;
+  icon:
+    | "alert"
+    | "arrow-left"
+    | "arrow-up"
+    | "arrow-right"
+    | "arrow-down"
+    | "ball"
+    | "clip"
+    | "club-sm"
+    | "comment"
+    | "counsel"
+    | "cross"
+    | "delete"
+    | "detail"
+    | "edit"
+    | "editor"
+    | "email"
+    | "facebook-sm"
+    | "hourglass"
+    | "internet-class"
+    | "list"
+    | "logout"
+    | "long-arrow-left"
+    | "long-arrow-up"
+    | "long-arrow-right"
+    | "long-arrow-down"
+    | "minus"
+    | "moon"
+    | "muffin"
+    | "notice"
+    | "ok"
+    | "ok-circle"
+    | "plus"
+    | "profile"
+    | "school-meal"
+    | "search"
+    | "service"
+    | "setting"
+    | "sun"
+    | "upload"
+    | "newspaper"
+    | "benedu"
+    | "between"
+    | "bamboo"
+    | "club-lg"
+    | "cart"
+    | "dets"
+    | "edison"
+    | "facebook-lg"
+    | "request"
+    | "research"
+    | "social"
+    | "submission";
+}
+
+const Dimigoincon = styled.span.attrs<IDimigoincon>(({ icon, className }) => ({
+  className: `icon-${icon} ${className}`,
+}))<IDimigoincon>``;
+
+export default Dimigoincon;
