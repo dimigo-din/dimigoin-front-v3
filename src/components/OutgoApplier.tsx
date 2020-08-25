81bbc83f1d50ab3475cf0a7f5ddaee4c65da1374
rycont <reactdev@kakao.com>
2020-08-25 23:25:03 +0900

    feat : OutgoApplier is modifiable

--- src/components/OutgoApplier.tsx
+++ src/components/OutgoApplier.tsx
@@ -1,40 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "./Chip";
 import Card from "./dimiru/DimiCard";
-import css from "@emotion/css";
+import styled from "@emotion/styled";
 
-interface IProps {
-  appliers: {
-    name: string;
-    studentId: string;
-  }[];
+interface Applier {
+  name: string;
+  studentId: string;
 }
 
-const OutgoApplier: React.FC<IProps> = ({ appliers }) => {
+const OutgoApplier: React.FC = () => {
+  const [appliers, setAppliers] = useState<Applier[]>([]);
+  const addPerson = (el: HTMLInputElement) => {
+    const text = el.value;
+    if (appliers) {
+      console.log(appliers);
+      setAppliers((beforeAppliers) => [
+        ...beforeAppliers!,
+        {
+          name: text,
+          studentId: text,
+        },
+      ]);
+      el.value = "";
+    }
+  };
   return (
     <Card leftBorder>
-      {appliers.map((applier) => (
-        <Chip key={applier.studentId}>
-          {applier.studentId} {applier.name}
-        </Chip>
-      ))}
-      <Chip
-        css={css`
-          margin-left: 12px;
-          width: 86px;
-          text-align: center;
-          background-color: transparent;
-          border: solid 2px #3c70e8;
-          color: #3c70e8;
-          font-size: 24px;
-          padding: 3px 24px;
-          vertical-align: middle;
-        `}
-      >
-        +
-      </Chip>
+      {appliers &&
+        appliers.map((applier) => (
+          <Chip key={applier.studentId}>
+            {applier.studentId} {applier.name}
+          </Chip>
+        ))}
+      <InputChip
+        placeholder="+"
+        onKeyDown={(e) =>
+          // @ts-ignore
+          e.key === "Enter" ? addPerson(e.target) : console.log(e.key)
+        }
+      />
     </Card>
   );
 };
+
+const InputChip = styled.input`
+  margin-left: 12px;
+  width: 86px;
+  text-align: center;
+  background-color: transparent;
+  border: solid 2px #3c70e8;
+  color: #3c70e8;
+  border-radius: 36px;
+  font-size: 16px;
+  padding: 0px 24px;
+  vertical-align: middle;
+  outline: none;
+  height: 36px;
+  line-height: 48px;
+  font-weight: 700;
+  &::placeholder {
+    color: #3c70e8;
+  }
+`;
 
 export default OutgoApplier;
