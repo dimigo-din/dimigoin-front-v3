02158437f00f3431502838d3df27725387f45f85
rycont <reactdev@kakao.com>
2021-01-01 21:37:47 +0900

    feat : add council router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,6 +6,7 @@ import Auth from "./Auth";
 import Ingangsil from "../pages/Ingangsil";
 import Outgo from "../pages/Outgo";
 import Notices from "../pages/Notices";
+import Council from "../pages/Council";
 
 const Router: React.FC = () => (
   <BrowserRouter>
@@ -15,6 +16,7 @@ const Router: React.FC = () => (
       <Route path="/outgo" component={Outgo} />
       <Route path="/notices/:articleId" component={Notices} />
       <Route path="/notices" component={Notices} />
+      <Route path="/council" component={Council} />
       <Route path="/" exact component={Main} />
     </Switch>
   </BrowserRouter>
