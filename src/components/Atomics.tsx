2e35ee1e3234b930a8ee9c076ce34408fc97605b
rycont <reactdev@kakao.com>
2021-01-01 21:35:18 +0900

    fix : explain card component extract

--- src/components/Atomics.tsx
+++ src/components/Atomics.tsx
@@ -6,20 +6,44 @@ export const UnstyledLink = styled(Link)`
   text-decoration: none;
 `
 
+export const Outlink = styled.a`
+  color: var(--main-theme-accent);
+  font-weight: 700;
+`
+
 export const Horizontal = styled.div`
   display: flex;
 `
 
-export const Description = styled.div`
+export const MyTodayDetail = styled.div`
   margin-left: 12px;
 `;
+
+export const DescriptionText = styled.p`
+  font-size: 16px;
+  color: #B8B8B8;
+  font-weight: 700;
+  line-height: 24px;
+`
 
 export const HeaderIconWrapper = styled.div`
   flex: 1;
   text-align: right;
   color: #8a8a8a;
 `
-
-// export const Highlighted = styled.div`
-//   background-color: rgba(60, 112, 232, 0.07);
-// `
+export const CardExplainContent = styled.div`
+  color: #8a8a8a;
+  font-size: 18px;
+  line-height: 30px;
+  & h2+p{
+    margin-top: 12px;
+  }
+  & h2 {
+    font-weight: 900;
+    font-size: 20px;
+    color: black;
+  }
+  & *+h2 {
+    margin-top: 12px;
+  }
+`;
