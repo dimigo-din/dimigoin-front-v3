b60a2b7fa800e4e4ca3938416debad2dd8eba6c0
rycont <reactdev@kakao.com>
2021-01-07 22:33:07 +0900

    fix : set min height

--- src/pages/SelfStudyDisplay/Timeline.tsx
+++ src/pages/SelfStudyDisplay/Timeline.tsx
@@ -58,19 +58,20 @@ export const Timeline: React.FC = () => {
     
     useEffect(() => console.log(timelineData), [ timelineData ])
 
-    return <div>
+    return <Wrapper>
         {timelineData?.map(timelineRow => <Timerow key={timelineRow.date.getMilliseconds().toString()}>
             <Time>[ {timelineRow.date.getHours().toString().padStart(2, '0')}:{timelineRow.date.getMinutes().toString().padStart(2, '0')} ]</Time> &nbsp;
             {timelineRow.subject.name}님({timelineRow.subject.studentId.slice(2)})이&nbsp;
             {timelineRow.target?.studentId ? <><Accent>{timelineRow.target.name}({timelineRow.target.studentId.slice(2)})</Accent>님</> : <Accent>본인</Accent>}의 현황을&nbsp;
             {timelineRow.location.from ? <><Accent>{timelineRow.location.from}</Accent>에서 <Accent>{timelineRow.location.to}</Accent>로 옮겼습니다.</> : <><Accent>{timelineRow.location.to}</Accent>로 등록했습니다</>}
         </Timerow>)}
-    </div>
+    </Wrapper>
 }
 
 const Timerow = styled.p`
     font-size: 22px;
     color: #9A9A9A;
+    line-height: 32px;
     &+& {
         margin-top: 24px;
     }
@@ -83,4 +84,9 @@ const Time = styled.time`
 const Accent = styled.span`
     color: var(--main-theme-accent);
     font-weight: 900;
+`
+
+const Wrapper = styled.div`
+    height: 50vh;
+    overflow-y: scroll;
 `
