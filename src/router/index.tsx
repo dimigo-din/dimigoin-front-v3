e1c4fb265ea167c9bc6c8ba1f69b6ee923d72893
rycont <reactdev@kakao.com>
2021-01-23 21:55:39 +0900

    refactor : restructure container hierarchy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Switch, Route, withRouter, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import { Main } from "../pages";
 import Auth from "./Auth";
@@ -11,6 +11,7 @@ import Council from "../pages/Council";
 import Mentoring from "../pages/Mentoring";
 import { LoadableComponent } from "@loadable/component";
 import { getToken } from "../api/auth";
+import styled from "@emotion/styled";
 
 const needAuth = (Component: LoadableComponent<{}>) => {
   return (params => {
@@ -23,16 +24,33 @@ const Router: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/auth" component={Auth} />
-      <Route path="/ingangsil" component={Ingangsil} />
-      <Route path="/outgo" component={Outgo} />
-      <Route path="/notices/:articleId" component={Notices} />
-      <Route path="/notices" component={Notices} />
-      <Route path="/selfstudydisplay" component={SelfStudyDisplay} />
-      <Route path="/council" component={Council} />
-      <Route path="/mentoring" component={Mentoring} />
-      <Route path="/" exact component={needAuth(Main)} />
+      <Container>
+        <TopLine />
+        <Route path="/ingangsil" component={Ingangsil} />
+        <Route path="/outgo" component={Outgo} />
+        <Route path="/notices/:articleId" component={Notices} />
+        <Route path="/notices" component={Notices} />
+        <Route path="/selfstudydisplay" component={SelfStudyDisplay} />
+        <Route path="/council" component={Council} />
+        <Route path="/mentoring" component={Mentoring} />
+        <Route path="/" exact component={needAuth(Main)} />
+      </Container>
     </Switch>
   </BrowserRouter>
 );
+
+const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  padding-bottom: 20px;
+`;
+
+export const TopLine = styled.div`
+  width: 100%;
+  height: 12px;
+  background-color: var(--main-theme-accent);
+`;
+
 
 export default Router;
