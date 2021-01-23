41521011f770de40a372a22f45da488432481dfa
rycont <reactdev@kakao.com>
2021-01-24 01:30:02 +0900

    feat : auth logic

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,7 @@
+import { toast } from 'react-toastify';
 import Cookies from 'universal-cookie';
 import { post } from './api';
-import { PostResource } from './serverResource';
+import { AuthTokens, PostResource } from './serverResource';
 const cookieStorage = new Cookies()
 
 enum STORAGE_KEY {
@@ -12,12 +13,28 @@ export const getToken = () => {
     return cookieStorage.get(STORAGE_KEY.ACCESS_TOKEN)
 }
 
-export const loginWithInfo = ({
+export const setTokens = (tokens: AuthTokens) => {
+    cookieStorage.set(STORAGE_KEY.ACCESS_TOKEN, tokens.accessToken)
+    cookieStorage.set(STORAGE_KEY.REFRESH_TOKEN, tokens.refreshToken)
+}
+
+export const clearTokens = () => {
+    cookieStorage.remove(STORAGE_KEY.ACCESS_TOKEN)
+    cookieStorage.remove(STORAGE_KEY.REFRESH_TOKEN)
+}
+
+export const loginWithInfo = async ({
     username,
     password
 } : PostResource['login']['req']) => {
-    post<"login">("login", {
-        password,
-        username
-    })
+    try {
+        const res = await post<"login">('/auth', {
+            password,
+            username
+        })
+        setTokens(res)
+        return true
+    } catch(e) {
+        return false
+    }
 }
