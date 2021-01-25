688b83dfd167d41cc5f334a3c3cbf40fed2a3922
rycont <reactdev@kakao.com>
2021-01-25 22:38:06 +0900

    chore : useMeal hook memoize

--- src/hooks/api/useMeal.ts
+++ src/hooks/api/useMeal.ts
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { DailyMeal, getDailyMeal } from "../../api"
 
 export const useMeal = (date?: Date) => {
     const [meal, setMeal] = useState<DailyMeal | null>(null)
-    useEffect(() => {
+    const dateKey = +(date||0)
+
+    useEffect((...d) => {
+        console.log('리렌더..', d)
         getDailyMeal(date).then(setMeal)
-    }, [date]);
+    }, [dateKey])
     return meal
 }
