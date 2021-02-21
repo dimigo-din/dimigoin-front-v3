f3ec019279dbd9ceb47230674306d33fdb9b8e27
rycont <reactdev@kakao.com>
2021-02-21 21:48:19 +0900

    feat : add card list

--- src/pages/Afterschool/Student.tsx
+++ src/pages/Afterschool/Student.tsx
@@ -1,25 +1,162 @@
-import React, { useEffect } from "react"
-import { CardGroupHeader, PageWrapper, ResponsiveWrapper } from "../../components"
+import css from "@emotion/css"
+import styled from "@emotion/styled"
+import React, { useCallback, useEffect, useState } from "react"
+import { getAfterschoolClassList } from "../../api"
+import { Card, CardGroupHeader, Col, Divider, PageWrapper, ResponsiveWrapper } from "../../components"
+import { dayEngKorMapper } from "../../constants"
+import { Doc, AfterschoolClass } from "../../constants/types"
 import useInput from "../../hooks/useInput"
+import { selfStudyTimesToString } from "../../utils"
 import { WeekDaySelector } from "./WeekDaySelector"
 
 const AfterschoolApply: React.FC = () => {
-    const weekDaySelectorInput = useInput<number>()
+    const [afterschoolClassList, setAfterschoolClassList] = useState<Doc<AfterschoolClass>[]>()
+    const weekDaySelectorInput = useInput<number | null>()
     const weekDaySelectorValue = weekDaySelectorInput.value
 
-    // useEffect(() => console.log(weekDaySelectorValue), [weekDaySelectorValue])
+    const fetchData = useCallback(() => {
+        getAfterschoolClassList()
+            .then(setAfterschoolClassList)
+    }, [setAfterschoolClassList])
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
 
     return <PageWrapper>
-        <CardGroupHeader
-            subButton={{
-                text: "신청한 강좌는 빨간 선으로 표시됩니다"
-            }}>
-                방과후
-            </CardGroupHeader>
         <ResponsiveWrapper>
-            <WeekDaySelector {...weekDaySelectorInput} />
+            <Col>
+
+                <CardGroupHeader
+                    subButton={{
+                        text: "신청한 강좌는 빨간 선으로 표시됩니다"
+                    }}>
+                    방과후
+                </CardGroupHeader>
+                <ResponsiveWrapper>
+
+                    <WeekDaySelector {...weekDaySelectorInput} />
+                    <Divider small data-divider />
+                    <CardGridWrapper>
+                        {afterschoolClassList?.map(afterschoolClass =>
+                            <>
+                                <ClassCard disableSpace leftBorder>
+                                    <CardHeaderWrapper>
+                                        <CardHeader>{afterschoolClass.name}</CardHeader>
+                                        <CountDisplay>{afterschoolClass.applierCount}/{afterschoolClass.capacity}명</CountDisplay>
+                                    </CardHeaderWrapper>
+                                    <CardDetailWrapper>
+                                        <CardDetail>{afterschoolClass.teacher.name} 선생님,</CardDetail>
+                                        <CardDetail>{afterschoolClass.days?.map(day => dayEngKorMapper[day]).join(' ')}요일,</CardDetail>
+                                        <CardDetail>{selfStudyTimesToString(afterschoolClass.times)}타임</CardDetail>
+                                    </CardDetailWrapper>
+                                    <CardContent>
+                                        {afterschoolClass.description}
+                                    </CardContent>
+                                    <ContentPopup>{afterschoolClass.description}</ContentPopup>
+                                </ClassCard>
+                            </>
+                        )}
+                    </CardGridWrapper>
+                </ResponsiveWrapper>
+
+            </Col><Divider small data-divider />
+            <Col>
+                <CardGroupHeader>
+                    신청목록
+                </CardGroupHeader>
+            </Col>
         </ResponsiveWrapper>
     </PageWrapper>
 }
 
 export default AfterschoolApply
+
+const ClassCard = styled(Card)`
+    margin: 12px;
+    width: 270px;
+    height: 210px;
+    box-sizing: border-box;
+`
+
+const CardGridWrapper = styled(ResponsiveWrapper)`
+    flex-wrap: wrap;
+    margin: -12px;
+    @media screen and (max-width: 900px) {
+        flex: 1;
+        flex-direction: column;
+        &>* {
+            width: unset !important;
+            flex: 1;
+        }
+    }
+`
+
+const CardDetailWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 6px;
+`
+
+const CardHeaderWrapper = styled.div`
+    display: flex;
+`
+
+const CardHeader = styled.h2`
+    font-size: 19px;
+    font-weight: 800;
+    flex: 1;
+    word-break: keep-all;
+    line-height: 24px;
+`
+
+const CountDisplay = styled(CardHeader)`
+    color: var(--main-theme-accent);
+    flex-shrink: 0;
+    flex-grow: 0;
+    white-space: nowrap;
+`
+
+const CardDetail = styled.p`
+    font-size: 17px;
+    font-weight: 700;
+    margin-top: 6px;
+    color: #393939;
+`
+
+const CardContent = styled.p`
+    font-size: 17px;
+    color: #707070;
+    margin-top: 15px;
+    line-height: 24px;
+    
+    overflow: hidden;
+    text-overflow: ellipsis;
+    
+    white-space: normal;
+    text-align: left;
+    word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+
+    &:hover+p {
+        opacity: 1;
+    }
+`
+
+const ContentPopup = styled.p`
+    transition: 300ms cubic-bezier(0, 0.76, 0.12, 0.98);
+    opacity: 0;
+    position: absolute;
+    width: 215px;
+    font-size: 17px;
+    background-color: #fff;
+    line-height: 24px;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.03);
+    padding: 12px;
+    box-sizing: border-box;
+    &:hover {
+        opacity: 1;
+    }
+`
---
+++ src/utils/formatSelfStudyTime.ts
@@ -0,0 +1,8 @@
+import { SelfStudyTime, SelfStudyTimeEngKor } from "../constants/types";
+
+export const selfStudyTimesToString = (times: SelfStudyTime[]): string =>
+    [...times]
+        .sort()
+        .map(time => SelfStudyTimeEngKor[time])
+        .reduce((acced, current) => acced[acced.length - 1]?.[0] === current[0] ? [...acced, current[current.length - 1]] : [...acced, current], [] as string[])
+        .join(' ')
